e3f52e099e11c3c48b75fbdf881398d3c633be86
Easycoding-art <vorpvnbvoron@gmail.com>
2024-06-12 02:41:06 +0300

    attributes are saved

--- cell_automat/cell_automat.py
+++ cell_automat/cell_automat.py
@@ -6,10 +6,10 @@ import os
 
 class Automat :
     def __init__(self, matrix) :
-        self.RES = matrix.shape
-        self.a, self.b = matrix.shape
-        self.matrix = matrix
-        self.colors = [(255,255,255), (0,0,0), (0,0,255), (0,255,0), (255,0,0), (255,100,10),
+        self.__RES = matrix.shape
+        self.__a, self.__b = matrix.shape
+        self.__matrix = matrix
+        self.__colors = [(255,255,255), (0,0,0), (0,0,255), (0,255,0), (255,0,0), (255,100,10),
                 (255,255,0), (0,255,170), (115,0,0), (180,255,100), (255,100,180), (240,0,255),
                 (127,127,127), (255,0,230), (100,40,0), (0,50,0), (0,0,100), (210,150,75),
                 (255,200,0), (255,255,100), (0,255,255), (200,200,200), (50,50,50), (230,220,170),
@@ -17,18 +17,18 @@ class Automat :
     
     def render(self, law, fps) :
         pg.init()
-        surface = pg.display.set_mode(self.RES)
+        surface = pg.display.set_mode(self.__RES)
         clock = pg.time.Clock()
         n = 0
         while True:
             if n != 0 :
-                previous_frame = self.matrix.copy()
-                self.matrix = law(previous_frame)
-                if np.array_equal(previous_frame, self.matrix) :
+                previous_frame = self.__matrix.copy()
+                self.__matrix = law(previous_frame)
+                if np.array_equal(previous_frame, self.__matrix) :
                     break
-            for i in range(self.a) :
-                for j in range(self.b) :
-                    surface.set_at((i, j), self.colors[self.matrix[i][j]])
+            for i in range(self.__a) :
+                for j in range(self.__b) :
+                    surface.set_at((i, j), self.__colors[self.__matrix[i][j]])
             pg.display.flip()
             clock.tick(fps)
             n+=1
@@ -38,16 +38,16 @@ class Automat :
         breaker = frames+1
         for n in range(frames+1) :
             if n != 0 :
-                previous_frame = self.matrix.copy()
-                self.matrix = law(previous_frame)
-                if np.array_equal(previous_frame, self.matrix) :
+                previous_frame = self.__matrix.copy()
+                self.__matrix = law(previous_frame)
+                if np.array_equal(previous_frame, self.__matrix) :
                     breaker = n
                     break
-            img  = Image.new( mode = "RGB", size = (self.a, self.b) )
+            img  = Image.new( mode = "RGB", size = (self.__a, self.__b) )
             pix = img.load()
-            for i in range(self.a) :
-                for j in range(self.b) :
-                    pix[i, j] = self.colors[int(self.matrix[i,j])]
+            for i in range(self.__a) :
+                for j in range(self.__b) :
+                    pix[i, j] = self.__colors[int(self.__matrix[i,j])]
             img.save('img_' + str(n) + '.jpg')
             images.append(ImageClip('img_' + str(n) + '.jpg').set_duration(duration))
         final_clip = concatenate_videoclips(images, method="compose")
--- cell_automat/effect_maker.py
+++ cell_automat/effect_maker.py
@@ -12,8 +12,8 @@ class EffectMaker :
         im = Image.open("frame.jpg") # Can be many different formats
         pix = im.load()
         s, h = im.size  # Get the width and hight of the image for iterating over
-        self.matrix = np.zeros((s, h))
-        self.colors = [(255,255,255), (0,0,0), (0,0,255), (0,255,0), (255,0,0), (255,100,10),
+        self.__matrix = np.zeros((s, h))
+        self.__colors = [(255,255,255), (0,0,0), (0,0,255), (0,255,0), (255,0,0), (255,100,10),
                 (255,255,0), (0,255,170), (115,0,0), (180,255,100), (255,100,180), (240,0,255),
                 (127,127,127), (255,0,230), (100,40,0), (0,50,0), (0,0,100), (210,150,75),
                 (255,200,0), (255,255,100), (0,255,255), (200,200,200), (50,50,50), (230,220,170),
@@ -21,16 +21,16 @@ class EffectMaker :
         
         for i in range(s) :
             for j in range(h) :
-                for n in range(len(self.colors)) :
-                    if ((pix[i, j][0] >= self.colors[n][0] - 50 and pix[i, j][0] <= self.colors[n][0] + 50) and
-                        (pix[i, j][1] >= self.colors[n][1] - 50 and pix[i, j][1] <= self.colors[n][1] + 50) and
-                        (pix[i, j][2] >= self.colors[n][2] - 50 and pix[i, j][2] <= self.colors[n][2] + 50)) :
-                        self.matrix[i, j] = n
+                for n in range(len(self.__colors)) :
+                    if ((pix[i, j][0] >= self.__colors[n][0] - 50 and pix[i, j][0] <= self.__colors[n][0] + 50) and
+                        (pix[i, j][1] >= self.__colors[n][1] - 50 and pix[i, j][1] <= self.__colors[n][1] + 50) and
+                        (pix[i, j][2] >= self.__colors[n][2] - 50 and pix[i, j][2] <= self.__colors[n][2] + 50)) :
+                        self.__matrix[i, j] = n
                         break
         os.remove("frame.jpg")
         
     def video(self, video_name, law, duration, frames, FPS) :
-        automat = Automat(self.matrix)
+        automat = Automat(self.__matrix)
         automat.get_video(law, video_name + "_effect", duration, frames, FPS)
         clip1 = VideoFileClip(self.video_file)
         clip2 = VideoFileClip(video_name + "_effect" + ".mp4")
